a0d16509516cd9067d60b4036081b78da3f3de1a
danword07 <92086319+danword07@users.noreply.github.com>
2021-10-07 13:28:41 +0300

    NewC#Script

---
+++ Robber Rivalry/Assets/ShitLayout.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShitLayout : MonoBehaviour
+{
+    public int braincellslost;
+    void Start()
+    {
+        braincellslost = 200;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
